a279912f08a1ab97cd318099ce31a46e51e5ee6b
Anil Kumar S <anil@lagoontechnologies.com>
2026-03-23 00:22:40 +0530

    WIP

--- 014-Running_Newman_With_Other_Ci_Servers_Or_Tools/002-Teamcity/Tests.js
+++ 014-Running_Newman_With_Other_Ci_Servers_Or_Tools/002-Teamcity/Tests.js
@@ -1,12 +1,14 @@
 /*
->>>> How to run Postman/Newman Tests in Teamcity.
-        https://medium.com/apis-with-valentine/how-to-run-postman-newman-tests-in-teamcity-9d767c7de542?skipOnboarding=1
->>>> Udemy video.
-        Please refer the actual udemy video for in-depth understanding.
-        Reference Link: https://www.udemy.com/course/postman-the-complete-guide/learn/lecture/10083256#overview
->>>> Postman newman teamcity repo.
-        Reference Link: https://github.com/vdespa/postman-newman-teamcity
->>>> Teamcity reporter.
-        Reference Link: https://www.npmjs.com/package/newman-reporter-teamcity
->>>>
-*/
+ * How to run Postman/Newman Tests in TeamCity.
+ *   Reference Link : https://medium.com/apis-with-valentine/how-to-run-postman-newman-tests-in-teamcity-9d767c7de542?skipOnboarding=1
+ *
+ * Udemy video.
+ *   Please refer the actual Udemy video for in-depth understanding.
+ *   Reference Link : https://www.udemy.com/course/postman-the-complete-guide/learn/lecture/10083256#overview
+ *
+ * Postman Newman TeamCity repo.
+ *   Reference Link : https://github.com/vdespa/postman-newman-teamcity
+ *
+ * TeamCity reporter.
+ *   Reference Link : https://www.npmjs.com/package/newman-reporter-teamcity
+ */
